c0b1bb3e65eac060d1ac668c0cb105bbd82fb977
Brendon Boldt <brendon.boldt@gmail.com>
2019-06-19 23:16:57 -0400

    Apply Black format; begin auxiliary classifier

--- frame_cnn.py
+++ frame_cnn.py
@@ -19,9 +19,11 @@ import world as W
 (6,) - fc
 """
 
+
 class Flatten(nn.Module):
     def forward(self, x):
         return x.view(x.size(0), -1)
+
 
 class Unflatten(nn.Module):
     def __init__(self, *new_shape):
@@ -30,6 +32,7 @@ class Unflatten(nn.Module):
 
     def forward(self, x):
         return x.view(x.size(0), *self.new_shape)
+
 
 class Noise(nn.Module):
     def __init__(self, dist):
@@ -41,70 +44,80 @@ class Noise(nn.Module):
             x += self.dist.sample(x.size()) - self.dist.mean
         return x
 
+
 class FrameCnn(nn.Module):
     def __init__(self):
         super(FrameCnn, self).__init__()
-        
-        in_size = (24,)
+
+        in_size = (100,)
         in_channels = 1
-        conv1_filters = 6
-        conv2_filters = 6
+        conv1_filters = 8
+        conv2_filters = 8
         flat_size = conv2_filters * in_size[0] // 4
         fc1_size = 36
-        hidden_size = 12
-        dropout_rate = 0.2
-        self.rohct = 10.
+        hidden_size = 20
+        dropout_rate = 0.3
+        self.rohct = 3.0
+
+        self.hidden_size = hidden_size
 
         # Batch normalization?
         self.encoder1 = nn.Sequential(
-                nn.Dropout(dropout_rate, True),
-                #Noise(torch.distributions.normal.Normal(0.0, 0.1)),
-                #Noise(torch.distributions.half_normal.HalfNormal(0.2)),
-                #Noise(torch.distributions.bernoulli.Bernoulli(0.1)),
-                #Noise(torch.distributions.relaxed_bernoulli.RelaxedBernoulli(1,probs=0.1)),
-                nn.Conv1d(in_channels, conv1_filters, 3, padding=1),
-                nn.ReLU(True),
-                nn.MaxPool1d(2, return_indices=True))
+            # nn.Dropout(dropout_rate, True),
+            # Noise(torch.distributions.normal.Normal(0.0, 0.1)),
+            # Noise(torch.distributions.half_normal.HalfNormal(0.05)),
+            # Noise(torch.distributions.bernoulli.Bernoulli(0.05)),
+            # Noise(torch.distributions.relaxed_bernoulli.RelaxedBernoulli(1,probs=0.1)),
+            nn.Conv1d(in_channels, conv1_filters, 5, padding=2),
+            nn.ReLU(True),
+            nn.MaxPool1d(2, return_indices=True),
+        )
         self.encoder2 = nn.Sequential(
-                nn.Conv1d(conv1_filters, conv2_filters, 3, padding=1),
-                nn.ReLU(True),
-                nn.MaxPool1d(2, return_indices=True))
+            nn.Conv1d(conv1_filters, conv2_filters, 5, padding=2),
+            nn.ReLU(True),
+            nn.MaxPool1d(2, return_indices=True),
+        )
         self.encoder3 = nn.Sequential(
-                Flatten(),
-                nn.Linear(flat_size, fc1_size),
-                nn.ReLU(True),
-                nn.Linear(fc1_size, hidden_size),)
-                #nn.Dropout(dropout_rate, True),)
-
-        #self.relaxed_categorical = torch.distributions.relaxed_categorical.RelaxedOneHotCategorical(torch.tensor([2.2])
+            Flatten(),
+            nn.Linear(flat_size, fc1_size),
+            nn.ReLU(True),
+            nn.Linear(fc1_size, hidden_size),
+            nn.Dropout(dropout_rate, True),
+        )
 
         self.decoder1 = nn.Sequential(
-                nn.Linear(hidden_size, fc1_size),
-                nn.ReLU(True),
-                nn.Linear(fc1_size, flat_size),
-                Unflatten(conv2_filters, in_size[0] // 4),)
+            nn.Linear(hidden_size, fc1_size),
+            nn.ReLU(True),
+            nn.Linear(fc1_size, flat_size),
+            Unflatten(conv2_filters, in_size[0] // 4),
+        )
         self.maxunpool1 = nn.MaxUnpool1d(2)
         self.decoder2 = nn.Sequential(
-                nn.ConvTranspose1d(
-                    conv2_filters,
-                    conv1_filters,
-                    3,
-                    padding=1),
-                nn.ReLU(True),) # a second relu?
+            nn.ConvTranspose1d(conv2_filters, conv1_filters, 3, padding=1),
+            nn.ReLU(True),
+        )  # a second relu?
         self.maxunpool2 = nn.MaxUnpool1d(2)
         self.decoder3 = nn.Sequential(
-                nn.ConvTranspose1d(conv2_filters, in_channels, 3, padding=1),
-                nn.Sigmoid())
+            nn.ConvTranspose1d(conv2_filters, in_channels, 3, padding=1), nn.Sigmoid()
+        )
 
-    def forward(self, x):
+    def apply_rohc(self, x):
+        if self.training:
+            return torch.distributions.relaxed_categorical.RelaxedOneHotCategorical(
+                torch.tensor([self.rohct]), logits=x
+            ).sample()
+        else:
+            s = x.shape
+            return torch.zeros(s).scatter_(-1, x.argmax(-1).view(s[:-1] + (1,)), 1.0)
+
+    def encode(self, x):
         x, mp_indices1 = self.encoder1(x)
         x, mp_indices2 = self.encoder2(x)
         x = self.encoder3(x)
-        if self.training:
-            x = torch.distributions.relaxed_categorical.RelaxedOneHotCategorical(torch.tensor([self.rohct]), logits=x).sample()
-        else:
-            s = x.shape
-            x = torch.zeros(s).scatter_(-1, x.argmax(-1).view(s[:-1] + (1,)), 1.)
+        return x, mp_indices1, mp_indices2
+
+    def forward(self, x):
+        x, mp_indices1, mp_indices2 = self.encode(x)
         x = self.decoder1(x)
         x = self.maxunpool1(x, mp_indices2)
         x = self.decoder2(x)
@@ -112,11 +125,27 @@ class FrameCnn(nn.Module):
         x = self.decoder3(x)
         return x
 
-'''
+
+class FCClassifier(nn.Module):
+    def __init__(self, frame_cnn):
+        ...
+        super(FCClassifier, self).__init__()
+        self.frame_cnn = frame_cnn
+        self.classifier = nn.Sequential(
+            nn.Linear(frame_cnn.hidden_size, num_classes), nn.Softmax()
+        )
+        # Get rid of softmax for training?
+
+    def forward(self, x):
+        x = self.frame_cnn.encode(x)[0]
+
+
+"""
 TODO
 - Dropout/sparsity/denoising
  - Add Gaussian noise (or whatever is appropriate) to input?
-'''
+"""
+
 
 def make_datasets(world_size, train_prop=0.8):
     data_dict = W.make_worlds(world_size)
@@ -125,13 +154,12 @@ def make_datasets(world_size, train_prop=0.8):
     print(f"total_size:\t{min_size*len(data_dict)}")
     for k, v in data_dict.items():
         data_dict[k] = random.sample(v, min_size)
-    
+
     train_size = int(min_size * train_prop)
-    train_ds = FrameCnnDataset(
-            { k: v[:train_size] for k, v in data_dict.items() })
-    test_ds = FrameCnnDataset(
-            { k: v[train_size:] for k, v in data_dict.items() })
+    train_ds = FrameCnnDataset({k: v[:train_size] for k, v in data_dict.items()})
+    test_ds = FrameCnnDataset({k: v[train_size:] for k, v in data_dict.items()})
     return train_ds, test_ds
+
 
 class FrameCnnDataset(D.Dataset):
     def __init__(self, data_dict):
@@ -140,7 +168,7 @@ class FrameCnnDataset(D.Dataset):
 
         """
         self.data_dict = data_dict
-        # Make sure we have a consistently ordered list of keys 
+        # Make sure we have a consistently ordered list of keys
         self.keys = list(data_dict.keys())
         self.list_len = len(data_dict[self.keys[0]])
 
@@ -153,88 +181,53 @@ class FrameCnnDataset(D.Dataset):
         return self.data_dict[sub_collection][sub_index].to_tensor()
 
 
-# I guess we didn't need this -- good practice, though!
-class GumbelSoftmax(nn.Module):
-    def __init__(self):
-        super(GumbelSoftmax, self).__init__()
-        self.uni = torch.distributions.uniform.Uniform(1e-7, 1.)
-        # -1, right?
-        self.softmax = torch.nn.Softmax(dim=-1)
-
-    def forward(self, logits):
-        # How do I stop the gradient here? Do I need to?
-        if self.training:
-            logits = -torch.log(-torch.log(self.uni.sample(logits.shape))) + logits
-            return self.softmax(logits)
-        else:
-            l = logits
-            s = logits.shape
-            return torch.zeros(s).scatter_(-1, l.argmax(-1).view(s[:-1] + (1,)), 1.)
-
-def main_gs():
-    gs = GumbelSoftmax()
-    x = torch.tensor([
-        [[1., 0, 0], [1,0,1]],
-        [[0, 1, 0], [1,1,1]]
-    ])
-    y = gs.forward(x)
-    for _ in range(100):
-        y += gs.forward(x)
-    print(x)
-    print()
-    print(y)
+class TwoFrameDataet(D.Dataset):
+    def __init__(self, data_dict):
+        ...
 
 
 def main():
     model = FrameCnn()
     metric = nn.MSELoss()
-    #metric = nn.L1Loss()
-    optimizer = torch.optim.Adam(
-            model.parameters(),
-            lr=1e-3,
-            weight_decay=1e-5)
+    # metric = nn.L1Loss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
 
-    world_size = 24
-    batch_size = 10
+    world_size = 100  # 24
+    batch_size = 20
+    # TODO better sampling here; too many examples?
     train_ds, test_ds = make_datasets(world_size, 0.8)
-    train_dl = D.DataLoader(
-            train_ds,
-            batch_size=batch_size,
-            shuffle=True)
-    test_dl = D.DataLoader(
-            test_ds,
-            batch_size=batch_size,
-            shuffle=True)
+    train_dl = D.DataLoader(train_ds, batch_size=batch_size, shuffle=True)
+    test_dl = D.DataLoader(test_ds, batch_size=batch_size, shuffle=True)
 
-    num_epochs = 100#60
+    num_epochs = 40
     for epoch in range(num_epochs):
+        losses = []
         for batch in train_dl:
             output = model(batch)
             loss = metric(output, batch)
+            losses.append(loss.detach().numpy())
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
-            model.rohct *= (1 - 5e-2)
-        print(f'epoch [{epoch+1}/{num_epochs}], '
-              f'loss: {loss:.4f}')
+        model.rohct *= 1 - 5e-1
+        print(f"epoch [{epoch+1}/{num_epochs}], loss: {np.average(losses):.4f}")
 
     model.eval()
     avg_loss = 0
     for batch in test_dl:
         output = model(batch)
-        if avg_loss == 0: # shorthand for first batch
+        if avg_loss == 0:  # shorthand for first batch
             for i in range(len(batch)):
                 print(W.show_fancy(batch[i]))
                 print(W.show_fancy(output[i]))
                 print()
         loss = metric(output, batch)
-        avg_loss += loss * len(batch)/(len(test_dl) * test_dl.batch_size)
+        avg_loss += loss * len(batch) / (len(test_dl) * test_dl.batch_size)
 
-    print(f'test loss: {avg_loss:.4f}')
-    torch.save(model.state_dict(), './frame_cnn.pth')
+    print(f"test loss: {avg_loss:.4f}")
+    torch.save(model.state_dict(), "./frame_cnn.pth")
 
 
-if __name__ == '__main__':
-    #main_gs()
+if __name__ == "__main__":
     main()
--- world.py
+++ world.py
@@ -9,6 +9,7 @@ World transitions
 - translate left/right
 """
 
+
 class WorldPair:
     def __init__(self, first, second):
         self.first = first
@@ -16,6 +17,7 @@ class WorldPair:
 
     def to_tensor():
         return torch.stack((self.first, self.second))
+
 
 class World1d:
     def __init__(self, length, depth):
@@ -29,7 +31,7 @@ class World1d:
         channel = 0
         for x, sz in self.blobs:
             for i in range(sz):
-                t[channel][i + x] = 1.
+                t[channel][i + x] = 1.0
         return t
 
     def __str__(self):
@@ -41,20 +43,24 @@ class World1d:
     def __eq__(self, other):
         return torch.eq(self.data, other.data).all()
 
+
 class SingleBlob(World1d):
     def __init__(self, length, depth, x, size):
         super(SingleBlob, self).__init__(length, depth)
         lo, hi = self.size_range
         self.size = int((lo + (hi - lo) * size) * self.length)
-        right = (self.length - self.size)
+        right = self.length - self.size
         self.x = int(right * x)
         self.blobs.append((self.x, self.size))
 
+
 class BigBlob(SingleBlob):
-    size_range = (6/24, 8.999/24)
+    size_range = (6 / 24, 8.999 / 24)
+
 
 class SmallBlob(SingleBlob):
-    size_range = (3/24, 4.999/24)
+    size_range = (3 / 24, 4.999 / 24)
+
 
 class DoubleBlob(World1d):
     def __init__(self, length, depth, x, sizes, sep):
@@ -65,100 +71,104 @@ class DoubleBlob(World1d):
         self.sizes = (s_1, s_2)
         lo, hi = self.sep_range
         self.sep = int((lo + (hi - lo) * sep) * self.length)
-        right = (self.length - sum(self.sizes) - self.sep)
+        right = self.length - sum(self.sizes) - self.sep
         self.x = int(right * x)
         x_1 = self.x + self.sizes[0] + self.sep
         self.blobs.append((self.x, self.sizes[0]))
         self.blobs.append((x_1, self.sizes[1]))
 
+
 class FarBlobs(DoubleBlob):
-    size_range = (3/24, 4.999/24)
-    sep_range = (5/24, 6.999/24)
+    size_range = (3 / 24, 4.999 / 24)
+    sep_range = (5 / 24, 6.999 / 24)
+
 
 class NearBlobs(DoubleBlob):
-    size_range = (3/24, 4.999/24)
-    sep_range = (2/24, 3.999/24)
+    size_range = (3 / 24, 4.999 / 24)
+    sep_range = (2 / 24, 3.999 / 24)
 
 
 def make_worlds(w_length):
-    d = {
-            'big_blob': [],
-            'small_blob': [],
-            'near_blobs': [],
-            'far_blobs': [],
-            }
+    d = {"big_blob": [], "small_blob": [], "near_blobs": [], "far_blobs": []}
     # TODO If this gets too big, generate via random index ordering
-    for size in (6, 7, 8):
+    # for size in (6, 7, 8):
+    for size in range(25, 40):
         for x in range(0, w_length - size + 1):
             w = World1d(w_length, 1)
             w.blobs.append((x, size))
-            d['big_blob'].append(w)
-    for size in (3, 4):
+            d["big_blob"].append(w)
+    # for size in (3, 4):
+    for size in range(8, 20):
         for x in range(0, w_length - size + 1):
             w = World1d(w_length, 1)
             w.blobs.append((x, size))
-            d['small_blob'].append(w)
+            d["small_blob"].append(w)
 
-    
-    for size1 in (3, 4):
-        for size2 in (3, 4):
-            for sep in (2, 3):
+    for size1 in range(8, 20):
+        for size2 in range(8, 20):
+            for sep in range(5, 15):
                 for x in range(0, w_length - (size1 + size2 + sep) + 1):
                     w = World1d(w_length, 1)
                     w.blobs.append((x, size1))
-                    w.blobs.append((x+size1+sep, size2))
-                    d['near_blobs'].append(w)
-    for size1 in (3, 4):
-        for size2 in (3, 4):
-            for sep in (5, 6):
+                    w.blobs.append((x + size1 + sep, size2))
+                    d["near_blobs"].append(w)
+    for size1 in range(8, 20):
+        for size2 in range(8, 20):
+            for sep in range(20, 40):
                 for x in range(0, w_length - (size1 + size2 + sep) + 1):
                     w = World1d(w_length, 1)
                     w.blobs.append((x, size1))
-                    w.blobs.append((x+size1+sep, size2))
-                    d['far_blobs'].append(w)
+                    w.blobs.append((x + size1 + sep, size2))
+                    d["far_blobs"].append(w)
 
     # ? Do the selection here or elsewhere?
     return d
-    
+
 
 show_blocks = [
-        (0.9, '█'),
-        (0.8, '▇'),
-        (0.7, '▆'),
-        (0.6, '▅'),
-        (0.5, '▄'),
-        (0.4, '▃'),
-        (0.3, '▂'),
-        (0.2, '▁'),
-        (-.1, ' '),
-        ]
+    (0.9, "█"),
+    (0.8, "▇"),
+    (0.7, "▆"),
+    (0.6, "▅"),
+    (0.5, "▄"),
+    (0.4, "▃"),
+    (0.3, "▂"),
+    (0.2, "▁"),
+    (-0.1, " "),
+]
+
 
 def show(w):
-    return "[" + "".join("█" if x > .9 else " " for x in w[0]) + "]"
+    return "[" + "".join("█" if x > 0.9 else " " for x in w[0]) + "]"
+
 
 def show_fancy(w):
     def to_block(x):
         for y, block in show_blocks:
-            if x > y: return block
+            if x > y:
+                return block
         return "X"
+
     # get the first channel of w
-    return "[" + ''.join(to_block(x) for x in w[0]) + "]"
+    return "[" + "".join(to_block(x) for x in w[0]) + "]"
+
 
 def blob(size, blob_size=6):
     w = torch.zeros(1, size)
     offset = random.randint(0, size - blob_size)
     for i in range(blob_size):
-        w[0][offset + i] = 1.
+        w[0][offset + i] = 1.0
     return w
+
 
 def blob_pair(size, blob_size=3):
     w = torch.zeros(1, size)
     # total world size, space for 2 blobs, 1 space of separation
     offset = random.randint(0, size - 2 * blob_size - 1)
     for i in range(blob_size):
-        w[0][offset + i] = 1.
+        w[0][offset + i] = 1.0
     # separation accounted for in the 1
-    offset_2 = random.randint(1, size - offset - 2*blob_size)
+    offset_2 = random.randint(1, size - offset - 2 * blob_size)
     for i in range(blob_size):
-        w[0][offset + blob_size + offset_2 + i] = 1.
+        w[0][offset + blob_size + offset_2 + i] = 1.0
     return w
